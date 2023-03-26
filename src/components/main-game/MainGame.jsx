265830b6c364279de26bee86721aba555ec077b1
sujitmemane <officialsujitmemane@gmail.com>
2023-03-26 12:00:16 +0530

    final

--- src/components/main-game/MainGame.jsx
+++ src/components/main-game/MainGame.jsx
@@ -3,49 +3,9 @@ import './main-game.css'
 import  {AiFillClockCircle} from 'react-icons/ai'
 import {RiNumbersFill } from 'react-icons/ri'
 import Confetti from 'react-confetti'
+import useTypo from '../../hooks/useTypo'
 const MainGame = () => {
-const [isStarted,setIsStarted]=useState(false)
-const [textInput,setTextInput]=useState('')
-const [wordCount,setWordCount]=useState(0)
-const [timeRemaning,setTimeRemaning]=useState(60)
-const [isOver,setIsOver]=useState(false)
-const inputRef=useRef()
-
-function textChangeHandler(event){
-  const {value}=event.target
-  setTextInput(value)
-}
-
-function countWords(text){
-  const totalWords=text.trim().split(' ').length
-  setWordCount(totalWords)
-}
-
-
-useEffect(()=>{
-  if(isStarted &&  timeRemaning>0){
-    setTimeout(()=>{
-      setTimeRemaning(prev=>prev-1)
-    },1000)
-
-  }
-  else if(timeRemaning===0){
-    countWords(textInput)
-    setIsStarted(false)
-    setIsOver(true)
-  
-  }
-},[timeRemaning,isStarted])
-
-function startGameHandler(){
-  setTimeRemaning(60)
-  setIsStarted(true)
-  setTextInput('')
-  setWordCount(0)
-  setIsOver(false)
-  inputRef.current.disabled = false
-  inputRef.current.focus()
-}
+  const { inputRef ,textChangeHandler,textInput,isStarted,isOver,startGameHandler,wordCount,timeRemaning} =useTypo()
   return (
     <section>
       <textarea
